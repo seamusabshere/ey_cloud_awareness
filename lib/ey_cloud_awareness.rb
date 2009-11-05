862748fb7a46cd07305ff8d5c3f0ad18cdccb000
Seamus Abshere <seamus@abshere.net>
2009-11-05 11:20:43 -0500

    Hack so that we don't interfere with aws-s3 by having required right_aws.
    right_http_connection's net_fix screws things up.

--- lib/ey_cloud_awareness.rb
+++ lib/ey_cloud_awareness.rb
@@ -1,11 +1,11 @@
 require 'open-uri'
 require 'set'
 require 'active_support'
-require 'right_aws'
+require 'right_aws' # See aws-s3 compatibility hack below
 require File.expand_path(File.join(File.dirname(__FILE__), 'engine_yard_cloud_instance'))
 
-# http://pragmatig.wordpress.com/2009/04/14/recursive-symbolize_keys/
 class Hash
+  # http://pragmatig.wordpress.com/2009/04/14/recursive-symbolize_keys/
   def recursive_symbolize_keys!
     symbolize_keys!
     values.select { |v| v.is_a?(Hash) }.each { |h| h.recursive_symbolize_keys! }
@@ -13,6 +13,35 @@ class Hash
   end
   
   def deep_copy
-    Marshal.load(Marshal.dump(self))
+    Marshal.load Marshal.dump(self)
+  end
+end
+
+# sabshere 11/05/09
+# Compatibility hack for aws-s3/right_aws
+# Apologies for rescuing instead of directly checking arity
+# I couldn't figure out how to do that because we don't have Module#instance_method
+# ... and the class has overridden Object#method (=> "GET"/"POST"/etc)
+module Net
+  class HTTPGenericRequest
+    def exec(sock, ver, path, send_only = nil)   #:nodoc: internal use only
+      if @body
+        begin
+          send_request_with_body sock, ver, path, @body, send_only
+        rescue ArgumentError
+          $stderr.puts "[EY CLOUD AWARENESS GEM] Rescued from #{$!} because we thought it might have to do with aws-s3/right_aws incompatibility"
+          send_request_with_body sock, ver, path, @body
+        end
+      elsif @body_stream
+        begin
+          send_request_with_body_stream sock, ver, path, @body_stream, send_only
+        rescue ArgumentError
+          $stderr.puts "[EY CLOUD AWARENESS GEM] Rescued from #{$!} because we thought it might have to do with aws-s3/right_aws incompatibility"
+          send_request_with_body_stream sock, ver, path, @body_stream
+        end
+      else
+        write_header sock, ver, path
+      end
+    end
   end
 end
